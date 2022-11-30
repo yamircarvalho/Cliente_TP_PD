bfed321e17d0b9f45b13a6082b4dc35b8569599c
alinotavares <a21230231@isec.pt>
2022-11-30 17:14:51 +0000

    Validação argumentos linha de comando cliente

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import ui.UIText;
 
 public class Main {
     public static void main(String[] args) {
+        if(args.length != 2){
+            System.err.println("Usage: java <name> <server port> <server address>");
+            return;
+        }
         Client client = new Client(Integer.parseInt(args[0]), args[1]);
 
         System.out.println(args[0] + args[1]);
